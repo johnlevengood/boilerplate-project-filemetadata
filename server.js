5c4a69d6a6807f57bb61fd82dfb9736d16dc9aaa
johnlevengood <50719877+johnlevengood@users.noreply.github.com>
2021-01-30 14:19:10 -0500

    first try

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer')
 var app = express();
+
+var storage = multer.memoryStorage()
+var upload = multer({storage: storage})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,17 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+    if (req.file) {
+        res.status(200).json({
+            name: req.file.originalname,
+            size: req.file.size,
+            type: req.file.mimetype
+        })
+    } else {
+        res.status(500).json({error: 'please upload a file first'})
+    }
+})
 
 
 const port = process.env.PORT || 3000;
